d0d83a05670519d5d99be207f59548a33ca3a48b
Arnab Sen <arnb.sen@hotmail.com>
2018-04-04 21:24:02 +0530

    Initial Commit

---
+++ TechFestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechFestApp
+//
+//  Created by Arnab Sen on 04/04/18.
+//  Copyright © 2018 Arnab Sen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
